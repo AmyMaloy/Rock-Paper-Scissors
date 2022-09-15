77ef8a695aa4f2f47e09bc53ea4088b3a8ce6859
amy <barsdorfa@gmail.com>
2022-09-15 06:21:26 -0400

    code refactor for UI

--- script.js
+++ script.js
@@ -1,134 +1,107 @@
-//gererates a random result
-function computerPlay(){
-    const pick = ['Rock','Paper', 'Scissors'];
-    return pick[Math.floor(Math.random() * pick.length)];
-}
-//console.log(computerPlay());
+//Cashing the Dom/storing variables for future use.
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const btn_div = document.querySelector("#refresh");
+
+//console.log("Game reset");
+//reload Game button
+btn_div.addEventListener("click", function() {
+   location.reload();
+});
 
 
-// the game start
-function game() {
-    let playerWin = 0;
-    let computerWin = 0;
-    let gameWinner = "";
-
-    //  Add event listeners for all three buttons/run round on click/track and end game
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', () => {
-            playerSelection = button.className;
-            const computerSelection = computerPlay();
-            battleWinText.textContent = (playRound(playerSelection, computerSelection));
-            playerWinText.textContent = "Player Win totals: " + playerWin;
-            computerWinText.textContent = "Computer Win totals: " + computerWin;
-            endGame();
-        })
-    })
-
-        // play the round and determine winner
-        function playRound(playerSelection, computerSelection) {
-            let tie = "It's a Tie! You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
-            let paperBeatRock = "You Win!  You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
-            let scissorsBeatPaperLoss = "You lose!  You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
-            let paperBeatRockLoss = "You lose!  You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
-            let rockBeatScissors = "You Win!  You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
-            let rockBeatScissorsLoss = "You lose!  You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
-            let scissorsBeatPaper = "You Win!  You selected " + playerSelection + " and the computer selected " + computerSelection + ".";
-
-            if (playerSelection === computerSelection) {
-                return tie;
-            } else if ((playerSelection === "Paper") && (computerSelection === "Rock")) {
-                playerWin++;
-                return paperBeatRock;
-            } else if ((playerSelection === "Paper") && (computerSelection === "Scissors")) {
-                computerWin++;
-                return scissorsBeatPaperLoss;
-            } else if ((playerSelection === "Rock") && (computerSelection === "Paper")) {
-                computerWin++;
-                return paperBeatRockLoss;
-            } else if ((playerSelection === "Rock") && (computerSelection === "Scissors")) {
-                playerWin++;
-                return rockBeatScissors;
-            } else if ((playerSelection === "Scissors") && (computerSelection === "Rock")) {
-                computerWin++;
-                return rockBeatScissorsLoss;
-            } else {
-                playerWin++;
-                return scissorsBeatPaper;
-            }
-        }
-    
-    //  create div DOM for all results
-    const container = document.querySelector("#container");
-    const resultsDiv = document.createElement("div");
-    resultsDiv.style.marginTop = "20px";
-    container.appendChild(resultsDiv);
-
-    //  create player win tracking DOM 
-    const playerWinText = document.createElement("p");
-    playerWinText.style.color = "blue";
-    playerWinText.textContent = "Player Win totals: " + playerWin;
-    resultsDiv.appendChild(playerWinText);
-
-    //  create computer win tracking DOM
-    const computerWinText = document.createElement("p");
-    computerWinText.style.color = "blue";
-    computerWinText.textContent = "Computer Win totals: " + computerWin;
-    resultsDiv.appendChild(computerWinText);
-
-    //  create battle win text DOM
-    const battleWinText = document.createElement("p");
-    battleWinText.style.color = "black";
-    resultsDiv.appendChild(battleWinText);
-    
-    //  create game win text DOM
-    const gameWinText = document.createElement("p");
-    gameWinText.style.color = "orange";
-    gameWinText.textContent = gameWinner;
-    resultsDiv.appendChild(gameWinText);
-
-    //  determine who won to five points first
-    function endGame() {
-        if (playerWin == 5) {
-            gameWinner = "YOU WIN!";
-            gameWinText.textContent = gameWinner;
-            
-            //  disable game buttons
-            document.getElementById("1").disabled = true;
-            document.getElementById("2").disabled = true;
-            document.getElementById("3").disabled = true;
-            
-            //  create new DOM button to replay
-            const playAgainButton = document.createElement("button");
-            playAgainButton.textContent = "Play Again!";
-            resultsDiv.appendChild(playAgainButton);
-            
-            //  if clicked, reload page
-            playAgainButton.addEventListener('click', () => {
-                location.reload();
-                })
-        } else if (computerWin == 5) {
-            gameWinner = "COMPUTER WINS!";
-            gameWinText.textContent = gameWinner;
-            
-            //  disable game buttons
-            document.getElementById("1").disabled = true;
-            document.getElementById("2").disabled = true;
-            document.getElementById("3").disabled = true;
-            
-            //  create new DOM button to replay
-            const playAgainButton = document.createElement("button");
-            playAgainButton.textContent = "Play Again!";
-            resultsDiv.appendChild(playAgainButton);
-            
-            //  if clicked, reload page
-            playAgainButton.addEventListener('click', () => {
-                location.reload();
-                })
-        }   
-    }
+//generates a random choice
+function getComputerChoice() {
+      const choices = ["r","p", "s"];
+      return choices[Math.floor(Math.random() * choices.length)];
 }
 
-//  function call to start the game
-game();
+function convertToWord(letter) {
+ if (letter === "r") return "Rock";
+ if (letter === "p") return "Paper";
+ return "Scissors";
+}
 
+//Declaring the winner or loser
+//what happens when win
+function win(userChoice, computerChoice) {
+   const smallUserWord = "user".fontsize(3).sup();
+   const smallCompWord = "comp".fontsize(3).sup();
+   const userChoice_div = document.getElementById(userChoice);
+   userScore++;
+   userScore_span.innerHTML = userScore;
+   computerScore_span.innerHTML = computerScore;
+   result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord}  beats  ${convertToWord(computerChoice)}${smallCompWord}. You win! 😄`;
+   userChoice_div.classList.add('green-glow');
+   setTimeout(function() {userChoice_div.classList.remove('green-glow') },300);
+}
+//what happens when lose
+function lose(userChoice, computerChoice) {
+   const smallUserWord = "user".fontsize(3).sup();
+   const smallCompWord = "comp".fontsize(3).sup();
+   const userChoice_div = document.getElementById(userChoice);
+   computerScore++;
+   userScore_span.innerHTML = userScore;
+   computerScore_span.innerHTML = computerScore;
+   result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord}  lose to ${convertToWord(computerChoice)}${smallCompWord}. You lost! 👎`;
+   userChoice_div.classList.add('red-glow');
+   setTimeout(function() {userChoice_div.classList.remove('red-glow') },300);
+}
+//what happens when draw
+function draw(userChoice, computerChoice) {
+   const smallUserWord = "user".fontsize(3).sup();
+   const smallCompWord = "comp".fontsize(3).sup();
+   const userChoice_div = document.getElementById(userChoice);
+   result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord}  same pick ${convertToWord(computerChoice)}${smallCompWord}. It's a draw!`;
+   userChoice_div.classList.add('gray-glow');
+   setTimeout(function() {userChoice_div.classList.remove('gray-glow') },300); 
+}
+//Game logic
+function game(userChoice) {
+ const computerChoice = getComputerChoice();
+ switch (userChoice + computerChoice) {
+   case "rs":
+   case "pr":
+   case "sp":
+      win(userChoice, computerChoice);
+      break;
+   case "rp":
+   case "ps":
+   case "sr":
+      lose(userChoice, computerChoice);
+      break;
+   case "rr":
+   case "pp":
+   case "ss":
+      draw(userChoice, computerChoice);
+      break;
+ } 
+}
+
+//Adding eventlisteners to buttons
+function main() {
+   rock_div.addEventListener("click", function() {
+    game("r");
+   })
+
+   paper_div.addEventListener("click", function() {
+    game("p");
+   })
+
+   scissors_div.addEventListener("click", function() {
+    game("s");
+   })
+}
+
+main();
+
+
+
+   
